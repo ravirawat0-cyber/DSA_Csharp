1b8448887dfa0f0e72b982c10b0e87b74603f33c
ravi rawat <ravirawat0@outlook.com>
2024-09-05 00:34:58 +0530

    Question: Count digit which divided n evenly.

---
+++ BasicsMaths/BasicsMaths/CountDigit.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace BasicsMaths
+{
+    internal class CountDigit
+    {
+
+        //Given a number n. Count the number of digits in n which evenly divide n.
+        //Return an integer, total number of digits of n which divides n evenly.
+        // Note :- Evenly divides means whether n is divisible by a digit i.e.
+        // leaves a remainder 0 when divided.
+        public int GetDigitCount(int number)
+        {
+            int count = 0;
+            int temp = number;
+
+            while (temp > 0)
+            {
+                int lastDigit = temp % 10;
+                if (number % lastDigit == 0)
+                    count++;
+                temp = temp / 10;
+            }
+
+            return count;
+
+        }
+    }
+}
+
---
+++ BasicsMaths/BasicsMaths/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace BasicsMaths
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var countDigit = new CountDigit();
+
+            Console.Write("Enter Digit to count: ");
+            var input = int.Parse(Console.ReadLine());
+            Console.WriteLine(countDigit.GetDigitCount(input));
+
+        }
+    }
+}
